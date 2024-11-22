7b7c1be143a9c9dcce3847bdcb86600ecc2a5b1e
Mauricio-mty <00000422@uca.edu.sv>
2024-11-21 23:26:35 -0600

    Inducción

--- tin-marin-App-V2/src/screens/InduccScreen.js
+++ tin-marin-App-V2/src/screens/InduccScreen.js
@@ -1,44 +1,91 @@
 import React  from 'react';
-import { StyleSheet,Text, View } from 'react-native';
+import { StyleSheet, Linking, View, TouchableOpacity, } from 'react-native';
+import { IconButton, Text, Title } from 'react-native-paper';
+import Colors from '../constants/Colors';
 
-
+/**
+*@param {prop} route
+*@param {prop} navigation 
+*@see https://reactnavigation.org/docs/route-prop/
+*@return {SafeAreaView}
+*/
 const InducScreen =  ({ navigation }) => {
-   /*
-    return(
-        <View style={StyleSheet.container}>
-          <Video 
-          
-               source={{
-                uri:'https://youtu.be/TQtT9QgWjIY'
-               }}
-               style={StyleSheet.backgroundVideo}
-               controls={true}
-               volume={10}
-               muted={true}
-          />              
+   
+  const guide="https://www.youtube.com/watch?v=VsjTbrmRkIs ";
+  const induccion="https://www.youtube.com/watch?v=AqtzkW-QBTo";
+  const doc="https://tinmarin-my.sharepoint.com/:b:/r/personal/srafael_tinmarin_org/Documents/Informaci%C3%B3n%20para%20App%20Formativa%20Tin%20Marin/Dem%C3%A1s%20informaci%C3%B3n/Inducci%C3%B3n/Inducci%C3%B3n%20Oficial%202024%20PDF.pdf?csf=1&web=1&e=tJHhot";
+
+
+   return(
+        <View style={styles.view}>
+          <Text style={styles.title}>Inducción Contenido</Text>
+        
+        <TouchableOpacity
+                  style={[styles.btnStyle]}
+                  onPress={() => {
+                    Linking.openURL(guide);
+                    }
+                  }>         
+              <Text style={styles.textStyle}>En los pies de un guia </Text>
+          </TouchableOpacity>     
+          <TouchableOpacity
+                  style={[styles.btnStyle]}
+                  onPress={() => {
+                    Linking.openURL(induccion);
+                    }
+                  }>         
+              <Text style={styles.textStyle}>Inducción del programa </Text>
+          </TouchableOpacity>     
+          <TouchableOpacity
+                  style={[styles.btnStyle]}
+                  onPress={() => {
+                    Linking.openURL(doc);
+                    }
+                  }>         
+              <Text style={styles.textStyle}>Inducción Oficial </Text>
+          </TouchableOpacity>     
+                      
         </View>
-    );*/
+    );
   
 
 };
 
 /*
-const styles = StyleSheet.create({
-    container:{
-        backgroundColor:'#F0EFEC',
-        flex:1,
-        alignItems:'center',
-        justifyContent:'center',
-    },
-    backgroundVideo:{
-        position:'absolute',
-        top:0,
-        left:0,
-        bottom:0,
-        right:0,
-        height:265,
-    },
-});
+ 
 */
+const styles = StyleSheet.create({
+  titleText: {
+    textAlign: 'center',
+    marginVertical: 20,
+    marginHorizontal: 15,
+  },
+  view: {
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  textStyle: {
+    color: 'white',
+    fontSize: 14,
+    paddingHorizontal: 25,
+  },
+  btnStyle: {
+    backgroundColor: Colors.primary,
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'center',
+    borderRadius: 10,
+    margin:10,
+    width: 220,
+    height: 40,
+  },
+  title: {
+    fontFamily: 'BubblegumSans-Regular',
+    marginTop: 20,
+    fontSize: 30,
+    color: '#566573',
+  }
+});
+
 
 export default InducScreen;
